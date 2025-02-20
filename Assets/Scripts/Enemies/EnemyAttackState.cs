2b08949860fad151c210fd98161d1f545eb5762d
Laugabr <125941878+Laugabr@users.noreply.github.com>
2025-02-20 19:33:02 -0300

    Implementacion de sistema de estados
    Solo creacoin y bases de los codigos, falta terminar

---
+++ Assets/Scripts/Enemies/EnemyAttackState.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class EnemyAttackState : EnemyBaseState
+{
+    public override  abstract void EnterState(EnemyStateManage enemy)
+    {
+
+    }
+    public override abstract void UpdateState(EnemyStateManage enemy)
+   {
+
+   }
+    public override abstract void OnCollisionEnter(EnemyStateManage enemy)
+   {
+
+   }
+}
