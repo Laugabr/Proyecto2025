d132778a55e61f2ba8eba0837f180c6478cc233d
Laugabr <125941878+Laugabr@users.noreply.github.com>
2025-02-25 16:47:19 -0300

    Skater arreglitos

--- Assets/Scripts/Enemies/Skater.cs
+++ Assets/Scripts/Enemies/Skater.cs
@@ -3,19 +3,31 @@ using UnityEngine;
 public class Skater : MonoBehaviour
 {
     public float speed = 5f;
-    private Vector2 direction = Vector2.right; // Empieza moviéndose a la derecha
+    public Vector2 direction = Vector2.right; // Empieza moviï¿½ndose a la derecha
 
     void Update()
     {
         transform.Translate(direction * speed * Time.deltaTime); // Movimiento
     }
 
-    private void OnTriggerEnter2D(Collider2D other) // Detecta si hubo colisión
+     /*
+   private void OnTriggerEnter2D(Collider2D other) // Detecta si hubo colisiï¿½n
     {
-        // Si choca con una pared, invierte la dirección
+        // Si choca con una pared, invierte la direcciï¿½n
         if (other.CompareTag("Wall"))
         {
-            direction *= -1;
+            direction *= new Vector2(-1,0);
         }
     }
+    */
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        // Si choca con una pared, invierte la direcciï¿½n
+        if (other.gameObject.CompareTag("Wall"))
+        {
+            direction *= new Vector2(-1,0);
+        }
+    }
+
 }
