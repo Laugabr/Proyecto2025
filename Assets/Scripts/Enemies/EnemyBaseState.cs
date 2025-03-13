48ea54eb6b3d3c2908694f868d9d2490ca23fb88
Laugabr <125941878+Laugabr@users.noreply.github.com>
2025-03-13 14:41:38 -0300

    EnemyAi
    Codificacion de la iA de los enemigos

--- Assets/Scripts/Enemies/EnemyBaseState.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-/*
-public  abstract class EnemyBaseState
-{
-   public  abstract void EnterState(EnemyStateManage enemy);
-   public abstract void UpdateState(EnemyStateManage enemy);
-   public abstract void OnCollisionEnter(EnemyStateManage enemy);
-}
-*/
